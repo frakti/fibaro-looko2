cd19e5a493eeea12e99201c4ada01ca7af407275
frakti <frakti7@gmail.com>
2021-01-24 15:22:01 +0100

    Add HTTP client to query LookO2 API

---
+++ ApiClient.lua
@@ -0,0 +1,37 @@
+class 'ApiClient'
+
+function ApiClient:new(token)
+    self.token = token
+    self.baseUrl = "http://api.looko2.com/"
+    return self
+end
+
+function ApiClient:getLastSensorMesurement(deviceId, success, error)
+    self:get("?method=GetLOOKO&id="..deviceId.."&token="..self.token, success, error)
+end
+
+function ApiClient:getAllSensors(success, error)
+    self:get("?method=GetAll&token="..self.token, success, error)
+end
+
+function ApiClient:getClosestSensor(lat, long, success, error)
+    self:get("?method=GPSGetClosestLooko&lat="..lat.."&lon="..long.."&token="..self.token, success, error)
+end
+
+function ApiClient:get(query, success, error)
+    local client = net.HTTPClient()
+    client:request(self.baseUrl..query, {
+        options = {
+            method = "GET",
+            headers = {
+                ["Accept"] = "application/json"
+            }
+        },
+        success = function (response)
+            QuickApp:debug(response.status)
+            QuickApp:debug(response.data)
+            success(json.decode(response.data))
+        end,
+        error = error
+    })
+end
