fd260ea074c4db4515d3aa1636a5f7acb4783575
frakti <frakti7@gmail.com>
2021-02-08 08:23:17 +0100

    Mean checker fixes

--- DailyParticleMeanChecker.lua
+++ DailyParticleMeanChecker.lua
@@ -26,47 +26,36 @@ function DailyParticleMeanChecker:record(recordDate, value)
 
   local countHoursAboveThreshold = 0
 
-  QuickApp:debug("[DNC] Measurement above norm", recordDate, value)
   local hourlyParticleAverages = self.settings:get("hourlyParticleAverages") or {}
-  QuickApp:debug("[DNC] Fetched averages above norm", json.encode(hourlyParticleAverages))
   local lastDayAverages = {}
 
   local canPersistNewRecord = true
-  for _, measurement in pairs(hourlyParticleAverages) do
+  for _, average in pairs(hourlyParticleAverages) do
 
-    -- all persisted measurements must be older than one hour of new records to make to persist it
-    QuickApp:debug("[DNC] IT:" .. _ .. "Check is after 1 hour", recordDate - measurement.d, recordDate - measurement.d > HOUR)
-    if (recordDate - measurement.d) < HOUR then
+    -- all persisted averages must be older than one hour of new records to make to persist it
+    if (recordDate - average.d) < HOUR then
       canPersistNewRecord = false
     end
 
-    -- drop measurements older than one day
-     QuickApp:debug("[DNC] IT:" .. _ .. "Check is from last 24 hours", os.time() - measurement.d  < DAY)
-    if ((os.time() - measurement.d) < DAY) then
-      table.insert(lastDayAverages, measurement)
+    -- drop averages older than one day
+    if ((os.time() - average.d) < DAY) then
+      table.insert(lastDayAverages, average)
 
-      if measurement.d < PM25_DAILY_MEAN then
+      if average.v > PM25_DAILY_MEAN then
         countHoursAboveThreshold = countHoursAboveThreshold + 1
       end
     end
-
   end
   if (canPersistNewRecord) then
     table.insert(lastDayAverages, {d = recordDate, v = value})
-    if value < PM25_DAILY_MEAN then
+    if value > PM25_DAILY_MEAN then
       countHoursAboveThreshold = countHoursAboveThreshold + 1
     end
   end
   self.settings:persist("hourlyParticleAverages", lastDayAverages)
 
-  -- trigger custom event if required
-  -- local countHoursAboveThreshold = rawlen(lastDayAverages)
-
-  QuickApp:debug("[DNC] Can send event", canPersistNewRecord, countHoursAboveThreshold >= exceededHoursThreshold, self:haveNotTriggeredWithinGivenHours(exceededHoursThreshold))
-
   if (countHoursAboveThreshold >= exceededHoursThreshold and self:haveNotTriggeredWithinGivenHours(exceededHoursThreshold)) then
      self.settings:persist("lastEventTriggerAt", os.time() - 1) -- decreasing to avoid too fast record and missed trigger
-    QuickApp:debug("[DNC] Trigger custom event")
     api.post("/customEvents/" .. EVENT_NAME)
   end
 end
